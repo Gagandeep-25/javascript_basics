8843c8c69313df7c4e286c08fbdc8eb1a7105da3
Gagandeep.L.S <gagandeepls7777@gmail.com>
2025-08-27 21:01:43 +0530

    Create condopp.js

---
+++ condopp.js
@@ -0,0 +1,38 @@
+console.log("conditional statements and opperators : ");
+
+//arithmatic opperations
+let a=10;
+let b=5;
+
+
+console.log("sum = ",a+b);
+console.log("difference =",a-b);
+console.log("product = ",a*b);
+console.log("quotient =",a/b);
+console.log("remainder = ",a%b);
+console.log("exponentiation = ",a**b);//10^5
+
+//unary opperators
+let x=1;
+let y=2;
+x++;
+y--;
+console.log("x=",++x,"y=",--y);
+
+//assignment operators 
+let c=4;
+console.log(c+=4)//c=c+4
+//now c=8
+console.log(c *= 4)//c=c*4
+
+let d="10";
+
+//comparision opperators 
+//==,===,!=,!===(returns a boolean value )
+console.log("a==b",a==b);
+console.log("a!=b",a==b);
+console.log("a==d",a==d);
+console.log("a===d",a===d);
+
+//logical opperators - return boolean value 
+//&&,||,!
